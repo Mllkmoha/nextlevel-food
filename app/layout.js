62c437d9fa834db878e31314ce0b4732119880b6
rafikmoha <m63866157@gmail.com>
2026-09-04 16:45:16 +0100

    Add Vercel Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next";
+
 import MainHeader from "@/components/main-header/main-Header";
 import "./globals.css";
 
@@ -12,6 +14,7 @@ export default function RootLayout({ children }) {
       <body>
         <MainHeader />
         {children}
+        <Analytics />
       </body>
     </html>
   );
