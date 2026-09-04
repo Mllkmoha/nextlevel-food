949b269d4c24ec39206ce35c8df9f160e09b20f1
rafikmoha <m63866157@gmail.com>
2026-09-04 16:47:07 +0100

    Add Vercel Speed Insights

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 
 import MainHeader from "@/components/main-header/main-Header";
 import "./globals.css";
@@ -15,6 +17,7 @@ export default function RootLayout({ children }) {
         <MainHeader />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
